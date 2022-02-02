577aa8c16e6ba7b18ceed822abfaca76d9416b7b
mhnaufal <aal.pirates16@gmail.com>
2022-02-02 11:56:32 +0700

    update pengguna controller

--- src/pengguna/pengguna.controller.ts
+++ src/pengguna/pengguna.controller.ts
@@ -1,40 +1,80 @@
 import { NextFunction, Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
 import logger from '@src/utils/logger';
-import { Pengguna } from './pengguna.entity';
+import { PenggunaRepository } from './pengguna.service';
 import { IPengguna } from './pengguna.interface';
 
 const getAllPengguna = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const penggunaService = getCustomRepository(PenggunaRepository);
+
   try {
-    const semuaPengguna: IPengguna[] | undefined = await Pengguna.find();
+    const semuaPengguna = await penggunaService.findAllPengguna();
+
     logger.info(semuaPengguna);
     res.json({ hasil: semuaPengguna });
   } catch (error) {
     logger.error(error);
     next(error);
-    throw new Error("Error accessing 'getAllPengguna' route");
+  }
+};
+
+const getPenggunaById = async (req: Request, res: Response, next: NextFunction) => {
+  const penggunaService = getCustomRepository(PenggunaRepository);
+  const idPengguna = req.params.id;
+
+  try {
+    const pengguna = await penggunaService.findByIdPengguna(idPengguna);
+
+    logger.info(pengguna);
+    res.json({ hasil: pengguna });
+  } catch (error) {
+    logger.error(error);
+    next(error);
   }
 };
 
 const createPengguna = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  const { nip, email, password, nama, alamat } = req.body;
-
-  const pengguna = Pengguna.create({
-    nip,
-    email,
-    password,
-    nama,
-    alamat,
-  });
+  const penggunaService = getCustomRepository(PenggunaRepository);
+  const penggunaData: IPengguna = req.body;
 
   try {
-    await pengguna.save();
+    const pengguna: IPengguna = await penggunaService.createPengguna(penggunaData);
     logger.info('Pengguna created succesfully');
     res.json({ hasil: pengguna });
   } catch (error) {
     logger.error(error);
     next(error);
-    throw new Error("Error accessing 'getAllPengguna' route");
   }
 };
 
-export { getAllPengguna, createPengguna };
+const updatePengguna = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const penggunaId = req.params.id;
+  const penggunaService = getCustomRepository(PenggunaRepository);
+  const penggunaData: IPengguna = req.body;
+
+  try {
+    const pengguna = await penggunaService.updatePengguna(penggunaId, penggunaData);
+
+    logger.info('Pengguna created succesfully');
+    res.json({ hasil: pengguna });
+  } catch (error) {
+    logger.error(error);
+    next(error);
+  }
+};
+
+const deletePengguna = async (req: Request, res: Response, next: NextFunction) => {
+  const penggunaService = getCustomRepository(PenggunaRepository);
+  const idPengguna = req.params.id;
+
+  try {
+    const pengguna = await penggunaService.deletePengguna(idPengguna);
+
+    logger.info(pengguna);
+    res.json({ hasil: pengguna });
+  } catch (error) {
+    logger.error(error);
+    next(error);
+  }
+};
+export { getAllPengguna, getPenggunaById, createPengguna, updatePengguna, deletePengguna };
