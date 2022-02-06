1f4bb402458e768235109c5f6c4bb0799e49265e
mhnaufal <aal.pirates16@gmail.com>
2022-02-06 09:20:22 +0700

    update route

--- src/home/home.route.ts
+++ src/home/home.route.ts
@@ -1,9 +1,17 @@
 import express, { Router } from 'express';
-import { renderLandingPage, renderLoginPage } from './home.controller';
+import { login, register } from '@src/authentication/authentication.controller';
+import { renderDashboard, renderLandingPage, renderLoginPage, renderRegisterPage } from './home.controller';
 
 const route: Router = express.Router();
 
 route.get('/', renderLandingPage);
+
+route.get('/register', renderRegisterPage);
+route.post('/register', register);
+
 route.get('/login', renderLoginPage);
+route.post('/login', login);
+
+route.get('/dashboard', renderDashboard);
 
 export { route as homeRoute };
