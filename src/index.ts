a798597da6c2998e03879c34e05a2d6c07719245
mhnaufal <aal.pirates16@gmail.com>
2022-02-06 21:06:35 +0700

    update index ts

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ import path from 'path';
 import express, { Express } from 'express';
 import { createConnection } from 'typeorm';
 import { engine } from 'express-handlebars';
+import session from 'express-session';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 /** @Utils */
+import { TypeormStore } from 'connect-typeorm';
+// eslint-disable-next-line import/default
+import passport from '@src/middlewares/passport.middleware';
 import logger from './utils/logger';
 import ormConfig from '../ormconfig';
 import HttpException from './utils/HttpException';
@@ -13,6 +17,7 @@ import errorMiddleware from './middlewares/error.middleware';
 /** @Route */
 import { homeRoute } from './home/home.route';
 import { penggunaRoute } from './pengguna/pengguna.route';
+import { Session } from './utils/Session.entity';
 
 dotenv.config();
 
@@ -24,44 +29,63 @@ const ENV: string = process.env.ENV || 'development';
 const app: Express = express();
 
 const server = async () => {
-  try {
-    /** Setting up the middlewares */
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(bodyParser.json());
-    app.use(express.static(path.join(__dirname, 'public')));
+  // try {
+  /** Setting up the middlewares */
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(express.static(path.join(__dirname, 'public')));
 
-    /** Setting up the view engine */
-    app.engine('hbs', engine({ extname: '.hbs', defaultLayout: 'main' }));
-    app.set('view engine', 'hbs');
-    app.set('views', path.join(__dirname, 'views'));
-    app.enable('view cache');
+  /** Setting up the view engine */
+  app.engine('hbs', engine({ extname: '.hbs', defaultLayout: 'main' }));
+  app.set('view engine', 'hbs');
+  app.set('views', path.join(__dirname, 'views'));
+  app.enable('view cache');
 
-    /** Database */
-    const db = await createConnection(ormConfig);
+  /** Database */
+  const db = await createConnection(ormConfig);
 
-    /** Routes */
-    app.use(homeRoute);
-    app.use(penggunaRoute);
+  /** Passport & Session */
+  const sessionRepository = db.getRepository(Session);
 
-    app.use((req, res, next) => {
-      const error = new HttpException(404, 'Not Found');
-      errorMiddleware(error, req, res, next);
-    });
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET || 'development',
+      resave: false,
+      saveUninitialized: false,
+      store: new TypeormStore({
+        cleanupLimit: 2,
+        limitSubquery: false,
+        ttl: 86400,
+      }).connect(sessionRepository),
+    })
+  );
 
-    /** Run server */
-    app.listen(PORT, HOST, () => {
-      logger.info('=========================================');
-      logger.info(`   🚀 App => ${APP_NAME}`);
-      logger.info('=========================================');
-      logger.info(`   🚀 Host => ${HOST}`);
-      logger.info(`   🚀 Environment => ${ENV}`);
-      logger.info(`   🚀 Port => ${PORT.toString()}`);
-      logger.info('=========================================');
-    });
-  } catch (error) {
-    logger.error(error);
-    throw new Error('❌ Unable to connect to the server ❌');
-  }
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  /** Routes */
+  app.use(homeRoute);
+  app.use(penggunaRoute);
+
+  app.use((req, res, next) => {
+    const error = new HttpException(404, 'Not Found');
+    errorMiddleware(error, req, res, next);
+  });
+
+  /** Run server */
+  app.listen(PORT, HOST, () => {
+    logger.info('=========================================');
+    logger.info(`   🚀 App => ${APP_NAME}`);
+    logger.info('=========================================');
+    logger.info(`   🚀 Host => ${HOST}`);
+    logger.info(`   🚀 Environment => ${ENV}`);
+    logger.info(`   🚀 Port => ${PORT.toString()}`);
+    logger.info('=========================================');
+  });
+  // } catch (error) {
+  // logger.error(error);
+  // throw new Error('❌ Unable to connect to the server ❌');
+  // }
 };
 
 server();
